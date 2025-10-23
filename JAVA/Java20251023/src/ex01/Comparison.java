d33ede48946d247fe7017efd12f39db2ad22cc05
jaden <everywhereworkplace@gmail.com>
2025-10-23 12:52:15 +0900

    20251023125215_commit

--- JAVA/Java20251023/src/ex01/Comparison.java
+++ JAVA/Java20251023/src/ex01/Comparison.java
@@ -59,10 +59,20 @@ public class Comparison {
          * 키보드 입력을 받는데 18세 이상이면 성인, 아니면 미성년자 출력
          */
         Scanner sc = new Scanner(System.in);
+
         System.out.println("나이를 입력하세요 : ");
         int age = sc.nextInt();
         String result = age >= 18 ? "성인" : "미성년자";
         System.out.println("당신은 " + result + " 입니다.");
+
+        /*
+         * 정수 입력을 받아서, 2로 나누어서 나머지가 0이면 짝수, 아니면 홀수
+         */
+
+        System.out.println("정수를 입력하세요 : ");
+        int numE = sc.nextInt();
+        String result2 = numE % 2 == 0 ? "짝수" : "홀수";
+        System.out.println(numE + "는 " + result2 + " 입니다.");
 
         sc.close();
 }
