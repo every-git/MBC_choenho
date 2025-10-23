e72eeefee84a2ffc5632b04ad67b9486c516fa35
jaden <everywhereworkplace@gmail.com>
2025-10-23 16:13:18 +0900

    20251023161318_commit

--- JAVA/Java20251023/src/ex01/If5.java
+++ JAVA/Java20251023/src/ex01/If5.java
@@ -2,7 +2,7 @@ package ex01;
 
 import java.util.Scanner;
 
-public class If4 {
+public class If5 {
 
     public static void main(String[] args) {
 
@@ -17,17 +17,25 @@ public class If4 {
         Scanner sc = new Scanner(System.in);
         System.out.println("점수를 입력하세요 : ");
         int number = sc.nextInt();
-
+        /*
+         * if 안에 if 중첩해서 조건을 추가하는 방식
+         */
         if (number >= 90) {
             System.out.println("A학점");
-        } else if (number >= 80) {
-            System.out.println("B학점");
-        } else if (number >= 70) {
-            System.out.println("C학점");
-        } else if (number >= 60) {
-            System.out.println("D학점");
         } else {
-            System.out.println("F학점");
+            if (number >= 80) {
+                System.out.println("B학점");
+            } else {
+                if (number >= 70) {
+                    System.out.println("C학점");
+                } else {
+                    if (number >= 60) {
+                        System.out.println("D학점");
+                    } else {
+                        System.out.println("F학점");
+                    }
+                }
+            }
         }
         System.out.println("-----프로그램 종료-----");
     }
---
+++ JAVA/Java20251023/src/ex01/If6.java
@@ -0,0 +1,56 @@
+package ex01;
+
+import java.util.Scanner;
+
+public class If6 {
+
+    public static void main(String[] args) {
+
+        /*
+         * 버스 요금 계산 프로그램
+         * 요금 : 2000
+         * 65세 이상 : 무료
+         * 20~64세 : 할인율 0%
+         * 15~19세 : 할인율 20%
+         * 7~14세 : 할인율 50%
+         * 6세 이하 : 무료
+         * 요금과 할인율이 변경이 되어도 쉽게 변경할 수 있게 만든다. (상수 사용)
+         * 문자가 입력되었을 시 예외 처리를 할 것.
+         */
+        // 기본 요금
+        int BASE_FARE = 2000;
+        // 나이
+        int SENIOR_AGE = 65;
+        int ADULT_AGE = 20;
+        int TEEN_AGE = 15;
+        int CHILD_AGE = 7;
+        int BABY_AGE = 6;
+        // 할인율
+        double SENIOR_RATE = 0;
+        double ADULT_RATE = 1.0;
+        double TEEN_RATE = 0.8;
+        double CHILD_RATE = 0.5;
+        // 입력받기
+        Scanner sc = new Scanner(System.in);
+        System.out.println("나이를 입력하세요 : ");
+        int age = sc.nextInt();
+        // 조건문
+        if (age >= SENIOR_AGE) {
+            System.out.println("65세 이상" + (int)(BASE_FARE * SENIOR_RATE) + "원 입니다.");
+        } else if (age >= ADULT_AGE) {
+            System.out.println("20~64세 " + (int)(BASE_FARE * ADULT_RATE) + "원 입니다.");
+        } else if (age >= TEEN_AGE) {
+            System.out.println("15~19세 " + (int)(BASE_FARE * TEEN_RATE) + "원 입니다.");
+        } else if (age >= CHILD_AGE) {
+            System.out.println("7~14세 " + (int)(BASE_FARE * CHILD_RATE) + "원 입니다.");
+        } else if (age <= BABY_AGE) {
+            System.out.println(BABY_AGE + "세 이하 무료 입니다.");
+        } else {
+            System.out.println("나이를 잘못 입력하였습니다.");
+        }   
+        // 프로그램 종료
+        System.out.println("-----프로그램 종료-----");
+        // 스캐너 닫기
+        sc.close();
+    }
+}
